edba65fbe96b8d3bcff3a007104f2c36bbc7d9fb
marinadavid5 <160735213+marinadavid5@users.noreply.github.com>
2024-03-20 09:58:23 -0300

    Add files via upload

---
+++ Exemplo.py
@@ -0,0 +1,40 @@
+print ("Marina")
+
+numero = 10
+nome = "João"
+booleano = True
+
+numero1 = 10
+numero2 = 55
+numero3 = 70
+numero4 = 125
+
+media = (numero1 + numero2 + numero3 + numero4) / 4
+print ("A média é", media)
+
+quadrado = numero1**2
+print ("O numero1 ao quadrado é", quadrado)
+
+dobro = numero2 * 2
+print ("O dobro do numero2 é", dobro)
+
+
+palavra = "Fluminense"
+letras = len(palavra)
+print ("Quantidade de letras da palavra = ", letras)
+
+frase = "Meu nome é Marina e sou tricolor!"
+qtd = frase.count(' ')
+print("Quantidade de espaços em branco na frase =", qtd)
+
+num = numero1 > numero2
+print ("O primeiro é maior que o segundo?", num)
+
+maior = max(numero1, numero2, numero3, numero4)
+print ("O maior número é", maior)
+
+
+
+
+
+
